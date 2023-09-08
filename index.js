cf262d5dcbdf743691d466a87afd37db51591956
ChrisK847 <cjohnk2@gmail.com>
2023-09-08 01:25:54 -0400

    Moved HTML files to sub-folder html, added images folder, javascript folder, videos folder, favicon.ico, css folder. Changed static paths to html files in index.html to relative paths. I also tried getting my background image to load, but it's not accepting local files for some reason

--- index.js
+++ index.js
@@ -8,50 +8,50 @@ var server = http.createServer(function (req, res) {
         switch (req.url) {
             case '/Create-Account.html':
                 console.log("Attempting to load '/Create-Account.html' | URL=" + req.url);
-                console.log(req);
-                fs.createReadStream("/Users/cjohn/Desktop/Desktop Items to Sort/Javascript/Volleyball-Sign-up/html/Create-Account.html", "UTF-8").pipe(res);
+                //console.log(req);
+                fs.createReadStream("./html/Create-Account.html", "UTF-8").pipe(res);
                 console.log("---------------");
                 break;
             case '/User-Profile-View.html':
                 console.log("Attempting to load '/User-Profile-View.html' | URL=" + req.url);
-                console.log(req);
-                fs.createReadStream("/Users/cjohn/Desktop/Desktop Items to Sort/Javascript/Volleyball-Sign-up/html/User-Profile-view.html", "UTF-8").pipe(res);
+                //console.log(req);
+                fs.createReadStream("./html/User-Profile-view.html", "UTF-8").pipe(res);
                 console.log("---------------");
                 break;
             case '/Sign-In.html':
                 console.log("Attempting to load '/Sign-In.html' | URL=" + req.url);
-                console.log(req);
-                fs.createReadStream("/Users/cjohn/Desktop/Desktop Items to Sort/Javascript/Volleyball-Sign-up/html/Sign-In.html", "UTF-8").pipe(res);
+                //console.log(req);
+                fs.createReadStream("./html/Sign-In.html", "UTF-8").pipe(res);
                 console.log("---------------");
                 break;
             case '/index.html':
                 console.log("Attempting to load '/index.html' | URL=" + req.url);
-                console.log(req);
-                fs.createReadStream("/Users/cjohn/Desktop/Desktop Items to Sort/Javascript/Volleyball-Sign-up/html/index.html", "UTF-8").pipe(res);
+                //console.log(req);
+                fs.createReadStream("./html/index.html", "UTF-8").pipe(res);
                 console.log("---------------");
                 break;
             case '/resources/demos/style.css':
                 console.log("Attempting to load '/resources/demos/style.css' | URL=" + req.url);
-                console.log(req);
-                fs.createReadStream("/Users/cjohn/Desktop/Desktop Items to Sort/Javascript/Volleyball-Sign-up/css/style.css", "UTF-8").pipe(res);
+                //console.log(req);
+                fs.createReadStream("./css/style.css", "UTF-8").pipe(res);
                 console.log("---------------");
                 break;
             case '/css/CSS-Reset.css':
                 console.log("Attempting to load '/css/CSS-Reset.css' | URL=" + req.url);
-                console.log(req);
-                fs.createReadStream("/Users/cjohn/Desktop/Desktop Items to Sort/Javascript/Volleyball-Sign-up/css/CSS-Reset.css", "UTF-8").pipe(res);
+                //console.log(req);
+                fs.createReadStream("./css/CSS-Reset.css", "UTF-8").pipe(res);
                 console.log("---------------");
                 break;
             case '/favicon.ico':
                 console.log("Attempting to load '/favicon.ico' | URL=" + req.url);
-                console.log(req);
-                fs.createReadStream("/Users/cjohn/Desktop/Desktop Items to Sort/Javascript/Volleyball-Sign-up/favicon.ico", "UTF-8").pipe(res);
+                //console.log(req);
+                fs.createReadStream("./favicon.ico", "UTF-8").pipe(res);
                 console.log("---------------");
                 break;
             default:
                 console.log("Default: Attempting to load '/index.htm'");
-                console.log(req);
-                fs.createReadStream("/Users/cjohn/Desktop/Desktop Items to Sort/Javascript/Volleyball-Sign-up/html/index.html", "UTF-8").pipe(res);
+                //console.log(req);
+                fs.createReadStream("./html/index.html", "UTF-8").pipe(res);
                 console.log("---------------");
                 break;
         }
